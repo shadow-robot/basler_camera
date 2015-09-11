bdb26bcfd8d33caf1cb8f6db9d3a77dd1272a22f
Beatriz Leon <beatriz@shadowrobot.com>
2015-09-11 16:53:34 +0100

    Clean up

--- include/image_publisher.h
+++ include/image_publisher.h
@@ -23,37 +23,11 @@ namespace Pylon
 
     ImagePublisher(ros::NodeHandle nh, sensor_msgs::CameraInfo::Ptr cinfo, string frame_id)
       : nh_(nh), it_(nh_) {
-      //  cinfo_manager_(nh) {
-      //cam_pub_ = it_.advertise("camera/image_raw", 1);
       cam_pub_ = it_.advertiseCamera("camera/image_raw", 1);
       converter_.OutputPixelFormat = PixelType_RGB8packed;
       cinfo_ = cinfo;
       frame_id_ = frame_id;
     }
-    /*    virtual void OnImageEventHandlerRegistered( CInstantCamera& camera)
-    {
-        ROS_INFO_STREAM("OnImageEventHandlerRegistered event for device " << camera.GetDeviceInfo().GetModelName());
-        std::cout << "OnOpened event for device " << camera.GetDeviceInfo().GetModelName() << std::endl;
-
-        INodeMap& nodemap = camera.GetNodeMap();
-
-        GenApi::CIntegerPtr width(camera.GetNodeMap().GetNode("Width"));
-        GenApi::CIntegerPtr height(camera.GetNodeMap().GetNode("Height"));
-
-        //ROS_INFO_STREAM("GetValue() " << width->GetValue());
-        ROS_INFO_STREAM("GetMin() " << width->GetMin());
-        //ROS_INFO_STREAM("GetMax() " << width->GetMax());
-        ROS_INFO_STREAM("width->GetInc() " << width->GetInc());
-
-        //ROS_INFO_STREAM("GetValue() " << height->GetValue());
-        ROS_INFO_STREAM("GetMin() " << height->GetMin());
-        //ROS_INFO_STREAM("GetMax() " << height->GetMax());
-        ROS_INFO_STREAM("width->GetInc() " << height->GetInc());
-
-        CEnumerationPtr pixelFormat( nodemap.GetNode( "PixelFormat"));
-        String_t oldPixelFormat = pixelFormat->ToString();
-        cout << "Old PixelFormat  : " << oldPixelFormat << endl;
-    }*/
 
     virtual void OnImagesSkipped( CInstantCamera& camera, size_t countOfSkippedImages)
     {
@@ -69,13 +43,9 @@ namespace Pylon
       GenApi::CIntegerPtr height(camera.GetNodeMap().GetNode("Height"));
       cv::Mat cv_img(width->GetValue(), height->GetValue(), CV_8UC3);
 
-      // Image grabbed successfully?
       if (ptrGrabResult->GrabSucceeded())
       {
         converter_.Convert(pylon_image_, ptrGrabResult);
-
-        ROS_DEBUG_STREAM("ptrGrabResult->GetHeight()" << ptrGrabResult->GetHeight());
-        ROS_DEBUG_STREAM("ptrGrabResult->GetWidth() " << ptrGrabResult->GetWidth());
 
         cv_img = cv::Mat(ptrGrabResult->GetHeight(),     ptrGrabResult->GetWidth(), CV_8UC3,(uint8_t*)pylon_image_.GetBuffer());
         sensor_msgs::ImagePtr image = cv_bridge::CvImage(std_msgs::Header(), "rgb8", cv_img).toImageMsg();
--- src/basler_camera_node.cpp
+++ src/basler_camera_node.cpp
@@ -11,51 +11,6 @@
 
 using namespace Pylon;
 
-// Adjust value to make it comply with range and increment passed.
-//
-// The parameter's minimum and maximum are always considered as valid values.
-// If the increment is larger than one, the returned value will be: min + (n * inc).
-// If the value doesn't meet these criteria, it will be rounded down to ensure compliance.
-int64_t Adjust(int64_t val, int64_t minimum, int64_t maximum, int64_t inc)
-{
-    // Check the input parameters.
-    if (inc <= 0)
-    {
-        // Negative increments are invalid.
-        throw LOGICAL_ERROR_EXCEPTION("Unexpected increment %d", inc);
-    }
-    if (minimum > maximum)
-    {
-        // Minimum must not be bigger than or equal to the maximum.
-        throw LOGICAL_ERROR_EXCEPTION("minimum bigger than maximum.");
-    }
-
-    // Check the lower bound.
-    if (val < minimum)
-    {
-        return minimum;
-    }
-
-    // Check the upper bound.
-    if (val > maximum)
-    {
-        return maximum;
-    }
-
-    // Check the increment.
-    if (inc == 1)
-    {
-        // Special case: all values are valid.
-        return val;
-    }
-    else
-    {
-        // The value must be min + (n * inc).
-        // Due to the integer division, the value will be rounded down.
-        return minimum + ( ((val - minimum) / inc) * inc );
-    }
-}
-
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "basler_camera");
@@ -67,14 +22,6 @@ int main(int argc, char* argv[])
   if( !priv_nh.getParam("frame_rate", frame_rate) )
     frame_rate = 20;
 
-  int cam_width;
-  if( !priv_nh.getParam("width", cam_width) )
-    cam_width = 1280;
-
-  int cam_height;
-  if( !priv_nh.getParam("height", cam_height) )
-    cam_height = 960;
-
   string camera_info_url;
   if( !priv_nh.getParam("camera_info_url", camera_info_url) )
     camera_info_url = "";
@@ -83,10 +30,9 @@ int main(int argc, char* argv[])
   if( !priv_nh.getParam("frame_id", frame_id) )
     frame_id = "";
 
-
   int exitCode = 0;
 
-  // Automagically call PylonInitialize and PylonTerminate to ensure the pylon runtime system
+  // Automatically call PylonInitialize and PylonTerminate to ensure the pylon runtime system
   // is initialized during the lifetime of this object.
   Pylon::PylonAutoInitTerm autoInitTerm;
   CGrabResultPtr ptrGrabResult;
@@ -109,34 +55,10 @@ int main(int argc, char* argv[])
 
     camera.RegisterImageEventHandler( new ImagePublisher(nh, cinfo, frame_id), RegistrationMode_Append, Cleanup_Delete);
 
-    // Register the standard configuration event handler for enabling software triggering.
     camera.RegisterConfiguration( new CAcquireContinuousConfiguration , RegistrationMode_ReplaceAll, Cleanup_Delete);
 
     camera.Open();
 
-    GenApi::CIntegerPtr width(camera.GetNodeMap().GetNode("Width"));
-    GenApi::CIntegerPtr height(camera.GetNodeMap().GetNode("Height"));
-
-    int64_t newWidth = cinfo->width;
-    newWidth = Adjust(newWidth, width->GetMin(), width->GetMax(), width->GetInc());
-
-    int64_t newHeight = cinfo->height;
-    newHeight = Adjust(newHeight, height->GetMin(), height->GetMax(), height->GetInc());
-
-    width->SetValue(newWidth);
-    height->SetValue(newHeight);
-
-    /*ROS_INFO_STREAM("GetValue() " << width->GetValue());
-    ROS_INFO_STREAM("GetMin() " << width->GetMin());
-    ROS_INFO_STREAM("GetMax() " << width->GetMax());
-    ROS_INFO_STREAM("width->GetInc() " << width->GetInc());
-
-    ROS_INFO_STREAM("GetValue() " << height->GetValue());
-    ROS_INFO_STREAM("GetMin() " << height->GetMin());
-    ROS_INFO_STREAM("GetMax() " << height->GetMax());
-    ROS_INFO_STREAM("width->GetInc() " << height->GetInc());*/
-
-    // Start the grabbing
     camera.StartGrabbing();
 
     while ( camera.IsGrabbing() && ros::ok())
