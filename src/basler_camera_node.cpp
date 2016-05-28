801e2fc2d8be5dcae7a1a07fac040c7ced9a13ab
contradict <contradict@gmail.com>
2016-05-28 15:49:37 -0700

    Support more standard parameter format.
    Placing all parameters in the basler_parameters map with explicit type
    information is still supported, but a warning will be printed. The more
    traditional mehod of placing the parameters at the top level and using the
    XmlRpc type information allows better compatibility dynamic_reconfigure.

--- src/basler_camera_node.cpp
+++ src/basler_camera_node.cpp
@@ -158,6 +158,67 @@ void handle_basler_parameters(CInstantCamera& camera)
   }
 }
 
+void handle_parameters(CInstantCamera& camera)
+{
+  ros::NodeHandle private_handle("~");
+  std::vector<std::string> keys;
+  std::string prefix = private_handle.getNamespace() + "/";
+  if(!private_handle.getParamNames(keys))
+  {
+      ROS_INFO("No params found on parameter server, using defaults from dynamic_reconfigure.");
+      return;
+  }
+  for(std::vector<std::string>::iterator k=keys.begin(); k != keys.end(); ++k)
+  {
+      if((*k).size() < prefix.size())
+      {
+          continue;
+      }
+      std::pair<std::string::iterator, std::string::iterator> different = std::mismatch(prefix.begin(), prefix.end(), (*k).begin());
+      if(different.first != prefix.end())
+      {
+          continue;
+      }
+      std::string param_name(different.second, (*k).end());
+      XmlRpc::XmlRpcValue v;
+      private_handle.getParam(*k, v);
+      if(param_name == "basler_params")
+      {
+          ROS_WARN("Using legacy basler_params value.");
+          handle_basler_parameters(camera);
+          break;
+      }
+      else if((param_name == "camera_info_url") ||
+              (param_name == "frame_id") ||
+              (param_name == "frame_rate") ||
+              (param_name == "serial_number"))
+      {
+          // handled in main before camera is opened
+          continue;
+      }
+      else if(v.getType() == XmlRpc::XmlRpcValue::TypeBoolean)
+      {
+          handle_basler_boolean_parameter(camera, param_name, v);
+      }
+      else if(v.getType() == XmlRpc::XmlRpcValue::TypeInt)
+      {
+          handle_basler_int_parameter(camera, param_name, v);
+      }
+      else if(v.getType() == XmlRpc::XmlRpcValue::TypeDouble)
+      {
+          handle_basler_float_parameter(camera, param_name, v);
+      }
+      else if(v.getType() == XmlRpc::XmlRpcValue::TypeString)
+      {
+          handle_basler_enum_parameter(camera, param_name, v);
+      }
+      else
+      {
+          ROS_ERROR_STREAM("Unexpected data type for " << param_name);
+      }
+  }
+}
+
 void configure_callback(basler_camera::CameraConfig &config, uint32_t level)
 {
     for (std::vector<basler_camera::CameraConfig::AbstractParamDescriptionConstPtr>::const_iterator _i = config.__getParamDescriptions__().begin(); _i != config.__getParamDescriptions__().end(); ++_i)
@@ -259,7 +320,7 @@ int main(int argc, char* argv[])
 
     camera.Open();
 
-    handle_basler_parameters(camera);
+    handle_parameters(camera);
 
     // Set the pixel format to RGB8 if available.
     INodeMap& nodemap = camera.GetNodeMap();
